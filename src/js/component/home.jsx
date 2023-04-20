4d3c896028a68dedae8028ecd1f6b2bc09516d74
sofiafernands <fernandesofia1802@gmail.com>
2023-04-20 23:08:52 +0000

    my traffic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React,{useState}from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
-export default Home;
+	const [colors , setColors]=useState("");
+	
+		function clicked(color){
+			console.log(color);
+			setColors(color);
+		}
+		
+	
+	
+		return (
+			<div className="text-center">
+				<div className="container text-center"  style={{backgroundColor:"black", height:"100px",width:"20px"}}></div>
+				<div className="container text-center"  style={{backgroundColor:"black", height:"400px",width:"150px",borderRadius:"25px"}}>
+				
+				<button onClick= {()=>clicked("red")}    style={{backgroundColor:"red", borderRadius:"50px",height:"20%",width:"65%",marginBottom:"30px", marginTop:"40px", boxShadow: colors === "red" ? "0px 0px 30px 20px white" : ""}} ></button>
+				<button onClick= {()=>clicked("yellow")} style={{backgroundColor:"yellow", borderRadius:"50px",height:"20%",width:"65%",marginBottom:"30px",boxShadow: colors === "yellow" ? "0px 0px 30px 20px white" : "" }}></button>
+				<button onClick= {()=>clicked("green")} style={{backgroundColor:"green", borderRadius:"50px",height:"20%",width:"65%",marginBottom:"30px",boxShadow: colors === "green" ? "0px 0px 30px 20px white" : ""}}></button>
+		</div>
+		</div>
+	
+				
+		);
+	}
+	
+	
+	export default Home;
+	
