f6d5ade9aeefdfa5ce8f3722279f6ae3aaa26d61
Sofia Fernandes <123645624+sofiafernands@users.noreply.github.com>
2023-07-20 15:50:37 +0000

    cambio de fondo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,6 @@
 import React,{useState}from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
 
 	const [colors , setColors]=useState("");
